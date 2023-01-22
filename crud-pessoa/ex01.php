9a665322bb6ad24a44ae5998720eb3071fcf2508
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-22 15:09:20 -0300

    a

--- crud-pessoa/ex01.php
+++ crud-pessoa/ex01.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$cmd = $this->pdo->prepare("INSERT INTO pessoa (nome, telefone, email) VALUES (:n, :t, :e)");
+$cmd = #pdo->prepare("INSERT INTO pessoa (nome, telefone, email) VALUES (:n, :t, :e)");
 $cmd->bindValue(":n" ,"Miriam");
 $cmd->bindValue(":t" ,"3199553374");
 $cmd->bindValue(":e" ,"Miriam@dmail.com");
